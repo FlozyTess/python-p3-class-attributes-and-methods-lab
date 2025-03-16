66ad2ac2f2ed91fa4861219a416e4f8fd98bf2de
FlozyTess <cecilrency23@gmail.com>
2025-03-16 20:00:47 +0300

    defined all attributes

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,8 @@
 class Song:
+    count = 0  # Class attribute to keep track of the number of songs
+    genres = []  # List to keep track of unique genres
+    artists = []  # List to keep track of unique artists
+    genre_count = {}  # Dictionary to keep count of each genre
+    artist_count = {}  # Dictionary to keep count of each artist
+
     pass
