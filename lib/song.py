0eec5dcbf89207b0685b1a5e6b089c988322e84f
FlozyTess <cecilrency23@gmail.com>
2025-03-16 22:18:09 +0300

    created class methods for the attributes

--- lib/song.py
+++ lib/song.py
@@ -4,5 +4,15 @@ class Song:
     artists = []  # List to keep track of unique artists
     genre_count = {}  # Dictionary to keep count of each genre
     artist_count = {}  # Dictionary to keep count of each artist
+    
+    def __init__(self, name, artist, genre):
+        self.name = name     #instance attributes
+        self.artist = artist
+        self.genre = genre
 
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
     pass
