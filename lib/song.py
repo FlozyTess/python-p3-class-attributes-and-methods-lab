67e5ff47f73289eee727e1b291ff8b12e386124c
FlozyTess <cecilrency23@gmail.com>
2025-03-17 00:11:49 +0300

    indented class metods correctly

--- lib/song.py
+++ lib/song.py
@@ -16,36 +16,36 @@ class Song:
         Song.add_to_genre_count(genre)
         Song.add_to_artist_count(artist)
 
-@classmethod    #increase the song count
-def add_song_to_count(cls):         
-    cls.count += 1          
+    @classmethod    #increase the song count
+    def add_song_to_count(cls):         
+        cls.count += 1          
         
 
-@classmethod   #adds unique genre
-def add_to_genres(cls, genre):          
-    if genre not in cls.genres:
-        cls.genres.append(genre)
+    @classmethod   #adds unique genre
+    def add_to_genres(cls, genre):          
+        if genre not in cls.genres:
+            cls.genres.append(genre)
 
-@classmethod    #adds unique artists
-def add_to_artists(cls, artist):        
-    if artist not in cls.artists:
-        cls.artists.append(artist)
+    @classmethod    #adds unique artists
+    def add_to_artists(cls, artist):        
+        if artist not in cls.artists:
+            cls.artists.append(artist)
 
-@classmethod   #Increment count if genre exists and initializes count if genre is new
-def add_to_genre_count(cls, genre):
-    if genre in cls.genre_count:
-        cls.genre_count[genre] += 1
-    else:
-        cls.genre_count[genre] = 1
+    @classmethod   #Increment count if genre exists and initializes count if genre is new
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
     
-@classmethod    #Increment count if artist exists and initializes count if artist is new
-def add_to_artist_count(cls, artist):
-    if artist in cls.artist_count:
-        cls.artist_count[artist] += 1
-    else:
-        cls.artist_count[artist] = 1    
+    @classmethod    #Increment count if artist exists and initializes count if artist is new
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1    
     
-    # 
+    
 ninety_nine_problems = Song("99 Problems", "Jay-Z", "Rap")
 hello = Song("Hello", "Adele", "Pop")
 beyonce_song = Song("Halo", "Beyonce", "Pop")
